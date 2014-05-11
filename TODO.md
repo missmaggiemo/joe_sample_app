feb82992a25c9dbcd503cf382179eaea79d73982
missmaggiemo <margaret.m.moreno@gmail.com>
2014-05-10 22:46:16 -0700

    gitignore

---
+++ TODO.md
@@ -0,0 +1,6 @@
+# TODO
+
+* stock photos that cycle through on front page
+
+* static login form, profile page, etc... links should all link to these static pages
+
--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -12,6 +12,8 @@
 //
 //= require jquery
 //= require jquery_ujs
+//= require jquery.ui.all
+//= require underscore
 //= require bootstrap
 //= require turbolinks
 //= require_tree .
